ae7db35f5014f8dce0806dea6b48a6e3af1a1a7b
saleamlak234 <salamlak101@gmail.com>
2025-06-29 09:26:44 +0300

    Updated auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -9,7 +9,7 @@ const telegramService = require('../services/telegram');
 const router = express.Router();
 
 // Email transporter setup
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER || 'sahamtrading11@gmail.com',
